82f18ca200680d85df11871c7db0ef5dc378862b
MAASAHEBBI USTAD <mhsrmy1@gmail.com>
2022-02-09 03:31:20 -0500

    8th pyt interview que

---
+++ pq8.py
@@ -0,0 +1,10 @@
+
+#8) What is Enumerate() Function in Python?
+
+'''The Python enumerate() function adds a counter to an iterable object. enumerate()
+function can accept sequential indexes starting from zero.'''
+
+
+subjects = ('Python', 'Interview', 'Questions')
+for i, subject in enumerate(subjects):
+ print(i, subject)
